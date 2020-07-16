2bc00707a9c7e7abeda4421820ca18eb30d2b66d
Bob King <bob@getready1.com>
2020-07-16 19:22:18 -0400

    Add random age to person

--- src/react-complete-guide/src/App.js
+++ src/react-complete-guide/src/App.js
@@ -11,6 +11,9 @@ class App extends Component {
         <h1> Hi, there.  I'm a react app.</h1>
         <p>This is really working.</p>
         <Person />
+        <Person />
+        <Person />
+        <Person />
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, there.  I\'m a React app!!!'));
