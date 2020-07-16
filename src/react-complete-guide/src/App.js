c39ac5c0d3164fb0443f08d035ce09ea909056b4
Bob King <bob@getready1.com>
2020-07-16 19:26:54 -0400

    Add props to pass data

--- src/react-complete-guide/src/App.js
+++ src/react-complete-guide/src/App.js
@@ -10,10 +10,11 @@ class App extends Component {
       <div className="App">
         <h1> Hi, there.  I'm a react app.</h1>
         <p>This is really working.</p>
-        <Person />
-        <Person />
-        <Person />
-        <Person />
+        <Person name="Max" age="28"/>
+        <Person name="Manu" age="29">My Hobbies: Racing</Person>
+        <Person name="Kate" age="13"/>
+        <Person name="Chloe" age="10"/>
+        
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, there.  I\'m a React app!!!'));
