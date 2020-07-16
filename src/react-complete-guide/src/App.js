3aa923822d1fbf6b4ad8b3da6a440bb675806c16
Bob King <bob@getready1.com>
2020-07-16 19:10:58 -0400

    Add Person folder

--- src/react-complete-guide/src/App.js
+++ src/react-complete-guide/src/App.js
@@ -6,6 +6,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1> Hi, there.  I'm a react app.</h1>
+        <p>This is really working.</p>
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, there.  I\'m a React app!!!'));
