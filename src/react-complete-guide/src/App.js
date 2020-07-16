f35cd2c59c66d1c406103697a1951dcdcd873f8a
Bob King <bob@getready1.com>
2020-07-16 19:37:46 -0400

    Add state information

--- src/react-complete-guide/src/App.js
+++ src/react-complete-guide/src/App.js
@@ -5,16 +5,27 @@ import Person from './Person/Person'
 
 
 class App extends Component {
+  state = {
+    persons: [
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Kate', age: 13 },
+      { name: 'Chloe', age: 10 }
+    ]
+  }
+
+
   render() {
     return (
       <div className="App">
         <h1> Hi, there.  I'm a react app.</h1>
         <p>This is really working.</p>
-        <Person name="Max" age="28"/>
-        <Person name="Manu" age="29">My Hobbies: Racing</Person>
-        <Person name="Kate" age="13"/>
-        <Person name="Chloe" age="10"/>
-        
+        <button>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+        <Person name={this.state.persons[3].name} age={this.state.persons[3].age} />
+
       </div>
     );
     // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, there.  I\'m a React app!!!'));
