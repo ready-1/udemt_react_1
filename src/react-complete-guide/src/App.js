afc2b48a00e9dd8f853ba1c39c7cbacc9578c195
Bob King <bob@getready1.com>
2020-07-16 19:05:54 -0400

    change back to jsx

--- src/react-complete-guide/src/App.js
+++ src/react-complete-guide/src/App.js
@@ -3,13 +3,12 @@ import './App.css';
 
 class App extends Component {
   render() {
-    // return (
-    //   <div className="App">
-    //     <h1> Hi, there.  I'm a react app.</h1>
-    //   </div>
-    // );
-    return React.createElement('div', { className: 'App' }, 
-            React.createElement('h1', null, 'Hi, there.  I\'m a React app!!!'));
+    return (
+      <div className="App">
+        <h1> Hi, there.  I'm a react app.</h1>
+      </div>
+    );
+    // return React.createElement('div', { className: 'App' }, React.createElement('h1', null, 'Hi, there.  I\'m a React app!!!'));
   }
 }
 
